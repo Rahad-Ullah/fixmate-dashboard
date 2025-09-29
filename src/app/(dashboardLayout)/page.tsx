c5d8b0e00922912357b3799c2ece74f920606f49
Rahad-Ullah <rahadullah10@gmail.com>
2025-09-29 17:33:36 +0600

    add: monthly earning chart

--- src/app/(dashboardLayout)/page.tsx
+++ src/app/(dashboardLayout)/page.tsx
@@ -1,63 +1,35 @@
 import ActiveUsersCard from "@/components/page/analytics/cards/ActiveUserCard";
-import ReportsCard from "@/components/page/analytics/cards/ReportsCard";
 import StatCard from "@/components/page/analytics/cards/StatCard";
-import CategoryChart from "@/components/page/analytics/charts/CategoryChart";
-import SalesVolumeChart from "@/components/page/analytics/charts/SalesVolumeChart";
-import UserGrowthChart from "@/components/page/analytics/charts/UserGrowthChart";
 import { Card } from "@/components/ui/card";
-import { DollarSign, ShoppingCart, TrendingUp, Users } from "lucide-react";
+import stateIcon_1 from "@/assets/icons/state-icon-1.svg";
+import stateIcon_2 from "@/assets/icons/state-icon-2.svg";
+import stateIcon_3 from "@/assets/icons/state-icon-3.svg";
+import stateIcon_4 from "@/assets/icons/state-icon-4.svg";
+import { EarningChart } from "@/components/page/analytics/charts/EarningChart";
 
 const AnalyticsPage = () => {
   return (
     <Card className="h-full p-5 animate-fadeIn flex flex-col gap-6">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-        <StatCard
-          title="Total Users"
-          value="24,521"
-          change="+12.5%"
-          isPositive={true}
-          icon={<Users className="h-6 w-6" />}
-          color="blue"
-        />
-        <StatCard
-          title="Active Users"
-          value="18,432"
-          change="+8.2%"
-          isPositive={true}
-          icon={<TrendingUp className="h-6 w-6" />}
-          color="green"
-        />
-        <StatCard
-          title="Total Products"
-          value="8,741"
-          change="+5.1%"
-          isPositive={true}
-          icon={<ShoppingCart className="h-6 w-6" />}
-          color="purple"
-        />
-        <StatCard
-          title="Total Revenue"
-          value="$1.2M"
-          change="+18.3%"
-          isPositive={true}
-          icon={<DollarSign className="h-6 w-6" />}
-          color="amber"
-        />
+        <StatCard title="Total Users" value="24,521" icon={stateIcon_1} />
+        <StatCard title="Total Provider" value="570" icon={stateIcon_2} />
+        <StatCard title="Upcoming Order" value="250" icon={stateIcon_3} />
+        <StatCard title="Total Revenue" value="$50,000" icon={stateIcon_4} />
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-        <UserGrowthChart />
+      <div className="grid grid-cols-[70%_auto] gap-6">
+        <EarningChart />
         <ActiveUsersCard />
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+      {/* <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
         <CategoryChart />
         <SalesVolumeChart />
-      </div>
+      </div> */}
 
-      <div className="grid grid-cols-1 gap-6">
+      {/* <div className="grid grid-cols-1 gap-6">
         <ReportsCard />
-      </div>
+      </div> */}
     </Card>
   );
 };
