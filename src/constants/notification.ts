47a8666b860dd00a0d64f0512c5d2ad72b27ae14
Rahad-Ullah <rahadullah10@gmail.com>
2025-10-02 20:34:18 +0600

    remove: unused components and files

--- src/constants/notification.ts
+++
@@ -1,2 +0,0 @@
-export const templateCategory = ["General", "Order", "Payment", "Returns"];
-export const templateStatuses = ["Active", "Draft"];
--- src/constants/report.ts
+++
@@ -1,1 +0,0 @@
-export const reportStauses = ["Pending", "In Review", "Resolved"];
