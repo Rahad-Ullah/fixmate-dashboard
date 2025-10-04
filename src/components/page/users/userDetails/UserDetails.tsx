46f20381708cc245729635dc0fbdb936d018cd01
Rahad-Ullah <rahadullah10@gmail.com>
2025-10-04 18:05:10 +0600

    modify: user details modal

--- src/components/page/users/userDetails/UserDetails.tsx
+++ src/components/page/users/userDetails/UserDetails.tsx
@@ -20,7 +20,7 @@ const UserDetails = ({ id }: { id: string }) => {
 
   return (
     <div className="grid gap-4">
-      <section className="flex gap-8 border-b pb-4">
+      <section className="flex items-center gap-8 border-b pb-4">
         <figure>
           <Image
             src={
@@ -29,7 +29,9 @@ const UserDetails = ({ id }: { id: string }) => {
             alt="avatar"
             width={150}
             height={150}
-            className="rounded-full"
+            className={`rounded-full aspect-square object-cover ${
+              userData?.role === "PROVIDER" ? "w-32 h-32" : "w-24 h-24"
+            }`}
           />
         </figure>
         <div className="flex flex-col justify-between">
@@ -42,126 +44,162 @@ const UserDetails = ({ id }: { id: string }) => {
             </h3>
           </div>
           {/* experience overview for provider only */}
-          <div className="grid grid-cols-1 md:grid-cols-3">
-            <div className="flex items-center gap-4 p-4 px-6 pl-0 border-r">
-              <Luggage size={28} strokeWidth={1.5} className="text-primary" />
-              <h4>
-                <span className="font-semibold">5+ Years</span> <br />{" "}
-                Experience
-              </h4>
+          {userData?.role === "PROVIDER" && (
+            <div className="grid grid-cols-1 md:grid-cols-3">
+              <div className="flex items-center gap-4 p-4 px-6 pl-0 border-r">
+                <Luggage size={28} strokeWidth={1.5} className="text-primary" />
+                <h4>
+                  <span className="font-semibold">
+                    {userData?.experience || "0 Year"}
+                  </span>
+                  <br /> Experience
+                </h4>
+              </div>
+              <div className="flex items-center gap-4 p-4 px-6 border-r">
+                <CircleCheckBig
+                  size={28}
+                  strokeWidth={2}
+                  className="text-primary"
+                />
+                <h4>
+                  <span className="font-semibold">
+                    {userData?.totalDoneWork || "0"}
+                  </span>
+                  <br /> Task Done
+                </h4>
+              </div>
+              <div className="flex items-center gap-4 p-4 px-6">
+                <Star size={28} strokeWidth={1.5} className="text-primary" />
+                <h4>
+                  <span className="font-semibold">
+                    {userData?.review || "0"}
+                  </span>
+                  <br /> Review
+                </h4>
+              </div>
             </div>
-            <div className="flex items-center gap-4 p-4 px-6 border-r">
-              <CircleCheckBig
-                size={28}
-                strokeWidth={2}
-                className="text-primary"
-              />
-              <h4>
-                <span className="font-semibold">150+</span> <br /> Task Done
-              </h4>
-            </div>
-            <div className="flex items-center gap-4 p-4 px-6">
-              <Star size={28} strokeWidth={1.5} className="text-primary" />
-              <h4>
-                <span className="font-semibold">4.9</span> <br /> Review
-              </h4>
-            </div>
-          </div>
+          )}
         </div>
       </section>
       {/* work overview for provider only */}
-      <section className="grid grid-cols-3 gap-4">
-        <div className="p-4 bg-white shadow-md rounded-lg">
-          <h4 className="text-xl text-center">
-            Completed Work <br />{" "}
-            <span className="text-2xl font-semibold text-[#008F37]">150</span>
-          </h4>
-        </div>
-        <div className="p-4 bg-white shadow-md rounded-lg">
-          <h4 className="text-xl text-center">
-            Upcoming Work <br />{" "}
-            <span className="text-2xl font-semibold text-[#F48201]">150</span>
-          </h4>
-        </div>
-        <div className="p-4 bg-white shadow-md rounded-lg">
-          <h4 className="text-xl text-center">
-            Cancel Work <br />{" "}
-            <span className="text-2xl font-semibold text-[#EB0000]">150</span>
-          </h4>
-        </div>
-      </section>
-      <section className="grid grid-cols-2 gap-4">
+      {userData?.role === "PROVIDER" && (
+        <section className="grid grid-cols-3 gap-4">
+          <div className="p-4 bg-white shadow-md rounded-lg">
+            <h4 className="text-xl text-center">
+              Completed Work <br />
+              <span className="text-2xl font-semibold text-[#008F37]">
+                {userData?.completedWork || "0"}
+              </span>
+            </h4>
+          </div>
+          <div className="p-4 bg-white shadow-md rounded-lg">
+            <h4 className="text-xl text-center">
+              Upcoming Work <br />
+              <span className="text-2xl font-semibold text-[#F48201]">
+                {userData?.upCommingWork || "0"}
+              </span>
+            </h4>
+          </div>
+          <div className="p-4 bg-white shadow-md rounded-lg">
+            <h4 className="text-xl text-center">
+              Cancel Work <br />
+              <span className="text-2xl font-semibold text-[#EB0000]">
+                {userData?.cancelWork || "0"}
+              </span>
+            </h4>
+          </div>
+        </section>
+      )}
+      <section
+        className={`grid ${
+          userData?.role === "PROVIDER" && "md:grid-cols-2"
+        } gap-4`}
+      >
         <div className="grid gap-4 h-fit">
           <div className="bg-white p-2 px-3 rounded-lg shadow-md">
-            <p className="flex justify-between items-center gap-4 py-3 border-b">
-              Gender <span>Male</span>
+            <p className="flex justify-between items-center gap-4 py-3 border-b capitalize">
+              Gender <span>{userData?.gender?.toLowerCase() || "Unknown"}</span>
             </p>
             <p className="flex justify-between items-center gap-4 py-3 border-b">
-              Date of Birth <span>01 January 2000</span>
+              Date of Birth <span>{userData?.dateOfBirth || "Unknown"}</span>
             </p>
             <p className="flex justify-between items-center gap-4 py-3">
-              Nationality <span>African</span>
+              Nationality <span>{userData?.nationality || "Unknown"}</span>
             </p>
           </div>
           <div className="bg-white p-2 px-3 rounded-lg shadow-md">
             <p className="flex justify-between items-center gap-4 py-3 border-b">
-              Mobile <span>+99123456789</span>
+              Mobile <span>{userData?.contact || "Unknown"}</span>
             </p>
             <p className="flex justify-between items-center gap-4 py-3 border-b">
-              Whatsapp <span>+99123456789</span>
+              Whatsapp <span>{userData?.whatsapp || "Unknown"}</span>
             </p>
             <p className="flex justify-between items-center gap-4 py-3">
-              E-mail <span>example@gmail.com</span>
+              E-mail <span>{userData?.email || "Unknown"}</span>
             </p>
           </div>
           <div className="bg-white p-2 px-3 rounded-lg shadow-md">
             <p className="flex justify-between items-center gap-4 py-3">
               Address
               <span className="text-right">
-                Flat 1204, Al Noor Tower, Sheikh Zayed Road, South Africa.
+                {userData?.address || "Unknown"}
               </span>
             </p>
           </div>
         </div>
-        <div className="grid gap-4 h-fit">
-          <div className="bg-white p-2 px-3 rounded-lg shadow-md">
-            <p className="flex justify-between items-center gap-4 py-3 border-b">
-              Expertise <span>Electrician</span>
-            </p>
-            <p className="flex justify-between items-center gap-4 py-3 border-b">
-              Country <span>South Africa</span>
-            </p>
-            <p className="flex justify-between items-center gap-4 py-3 border-b">
-              Service Area <span>Cape Town</span>
-            </p>
-            <p className="flex justify-between items-center gap-4 py-3 border-b">
-              Service Distance <span>20km</span>
-            </p>
-            <p className="flex justify-between items-center gap-4 py-3 border-b">
-              Available Days <span>Sat, Sun, Fri</span>
-            </p>
-            <p className="flex justify-between items-center gap-4 py-3">
-              Available Time <span>10am - 6pm</span>
-            </p>
+        {/* work overview for provider only */}
+        {userData?.role === "PROVIDER" && (
+          <div className="grid gap-4 h-fit">
+            <div className="bg-white p-2 px-3 rounded-lg shadow-md">
+              <p className="flex justify-between items-center gap-4 py-3 border-b">
+                Expertise <span>{userData?.expertise || "Unknown"}</span>
+              </p>
+              <p className="flex justify-between items-center gap-4 py-3 border-b">
+                Country <span>{userData?.country || "Unknown"}</span>
+              </p>
+              <p className="flex justify-between items-center gap-4 py-3 border-b">
+                Service Area <span>{userData?.serviceArea || "Unknown"}</span>
+              </p>
+              <p className="flex justify-between items-center gap-4 py-3 border-b">
+                Service Distance (km)
+                <span>{userData?.serviceDistance || "Unknown"}</span>
+              </p>
+              <p className="flex justify-between items-center gap-4 py-3 border-b">
+                Available Days
+                <span className="capitalize">
+                  {userData?.availableDay
+                    ?.map((day) => day.toLowerCase())
+                    .join(", ") || "Unknown"}
+                </span>
+              </p>
+              <p className="flex justify-between items-center gap-4 py-3">
+                Available Time{" "}
+                <span>
+                  {`${userData?.availableTime?.startTime} - ${userData?.availableTime?.endTime}` ||
+                    "Unknown"}
+                </span>
+              </p>
+            </div>
+            <div className="bg-white p-2 px-3 rounded-lg shadow-md">
+              <p>
+                <span className="font-semibold">Overview:</span> <br />{" "}
+                <span className="text-sm">
+                  {userData?.overview || "Unknown"}
+                </span>
+              </p>
+            </div>
+            {/* licenses files */}
+            {userData?.licenses?.length > 0 &&
+              userData?.licenses?.map((license: string, idx: number) => (
+                <div key={idx} className="bg-white p-3 rounded-lg shadow-md">
+                  <p className="flex justify-between items-center gap-4">
+                    <span>{license || "Unknown"}</span>
+                    <File className="text-red-500" />
+                  </p>
+                </div>
+              ))}
           </div>
-          <div className="bg-white p-2 px-3 rounded-lg shadow-md">
-            <p>
-              <span className="font-semibold">Overview:</span> <br />{" "}
-              <span className="text-sm">
-                I am a certified electrician with over 5 years of proven
-                experience handling both residential and commercial electrical
-                projects. Throughout my career. installations, troubleshooting,
-                repairs, and regular maintenance.
-              </span>
-            </p>
-          </div>
-          <div className="bg-white p-3 rounded-lg shadow-md">
-            <p className="flex justify-between items-center gap-4">
-              <span>License.pdf</span>
-              <File className="text-red-500" />
-            </p>
-          </div>
-        </div>
+        )}
       </section>
     </div>
   );
